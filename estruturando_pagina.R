425d55d35fc2170c2fd7890d4ab7a41ea3820e65
LuizPaulo23 <luizpauloueg@gmail.com>
2023-02-11 00:27:08 -0300

    ADD: estruturação de página

---
+++ estruturando_pagina.R
@@ -0,0 +1,42 @@
+rm(list = ls())
+
+# Packages/bibliotecas 
+
+library(shiny)
+library(tidyverse)
+
+# Primeiros Passos - Aula I
+
+ui <- shiny::fluidPage(
+  titlePanel("Hello, Shiny")
+)
+
+server <- function(input, output){
+  
+}
+
+shiny::shinyApp(ui, server)
+
+# Aula II - Estruturação de página 
+
+ui = shiny::fluidPage(
+  
+  titlePanel("Estruturação"), # um parâmetro
+  sidebarLayout(position = "left",
+    sidebarPanel(h5("Nessa área ficarão os dados de input")
+                 
+      ),
+    
+    mainPanel(h5("Aqui ficarão colocados os elementos de saída")
+   ),
+  
+ )
+  
+)
+
+server <- function(input, output){
+  
+}
+
+shiny::shinyApp(ui, server = server)
+
